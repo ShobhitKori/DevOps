731f7c2940e5e69f0557283c30172d0f1decdbdd
ShobhitKori <112234874+ShobhitKori@users.noreply.github.com>
2024-06-13 21:16:14 +0530

    Added endpoint

--- main.py
+++ main.py
@@ -14,3 +14,16 @@ def about():
 def contact_us():
 	return {"email": "abc@gmail.com", "mob" : "1234567890"}
 
+@app.get("/login")
+def login_get(username : str, password : str):
+	if username == "dev" and password == "123456":
+	  return {"data": "This is login page", "mob" : "1234567890"}
+	else :
+		return {"data" : "Login failed incorrect username or password"}
+
+@app.post("/login")
+def login_post(username : str, password : str):
+	if username == "dev" and password == "123456":
+	  return {"data": "This is login page", "mob" : "1234567890"}
+	else :
+	  return {"data" : "Login failed i
